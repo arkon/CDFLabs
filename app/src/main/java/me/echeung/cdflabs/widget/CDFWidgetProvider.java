1cad6a0856ced4c4c4ac896f7fb92e75ab35ea58
Eugene Cheung <ey.cheung@outlook.com>
2014-08-12 16:36:43 -0400

    Rough widget code

---
+++ app/src/main/java/me/echeung/cdflabs/widget/CDFWidgetProvider.java
@@ -0,0 +1,53 @@
+package me.echeung.cdflabs.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import java.util.Random;
+
+import me.echeung.cdflabs.R;
+
+/**
+ * Created by Eugene on 2014-08-12.
+ */
+public class CDFWidgetProvider extends AppWidgetProvider {
+
+    private static final String ACTION_CLICK = "ACTION_CLICK";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+
+        // Get all ids
+        ComponentName thisWidget = new ComponentName(context,
+                CDFWidgetProvider.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+        for (int widgetId : allWidgetIds) {
+            // create some random data
+            int number = 10;
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.appwidget_layout);
+
+            // Set the text
+            remoteViews.setTextViewText(R.id.update, String.valueOf(number));
+
+            // Register an onClickListener
+            Intent intent = new Intent(context, CDFWidgetProvider.class);
+
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+}
