237c9c577ff592b067081ae43f4e728ce98d40c7
Eugene Cheung <ey.cheung@outlook.com>
2016-01-06 23:25:53 -0500

    Remove unused imports

--- app/src/main/java/me/echeung/cdflabs/utils/PrinterDataScraper.java
+++ app/src/main/java/me/echeung/cdflabs/utils/PrinterDataScraper.java
@@ -3,15 +3,9 @@ package me.echeung.cdflabs.utils;
 import android.os.AsyncTask;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
 
 import org.jsoup.Jsoup;
 
-import java.lang.reflect.Type;
 import java.util.Collections;
 
 import me.echeung.cdflabs.adapters.ViewPagerAdapter;
